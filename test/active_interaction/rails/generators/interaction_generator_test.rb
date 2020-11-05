b4df397d78fc02529a0ce95fdd756539aa424e10
Ngari Ndung'u <ngari.ndungu@gmail.com>
2020-11-05 23:13:56 +0300

    add sample test cases

--- test/active_interaction/rails/generators/interaction_generator_test.rb
+++ test/active_interaction/rails/generators/interaction_generator_test.rb
@@ -8,11 +8,27 @@ class ActiveInteraction::Rails::Generators::InteractionGeneratorTest < ::Rails::
   destination File.expand_path('../tmp', __dir__)
   setup :prepare_destination
 
-  def test_files_get_created
-    assert false
+  def test_creates_interaction
+    # skip
+    run_generator %w[signup]
+    assert_file 'app/interactions/signup_interaction.rb', /ActiveInteraction::Base/
   end
 
-  # def prepare_destination
-  #   p 'prepared'
-  # end
+  def test_creates_nested_interaction
+    skip
+    run_generator %w[users/signup]
+    assert_file 'app/interactions/users/signup_interaction.rb', /ActiveInteraction::Base/
+  end
+
+  def test_interaction_with_attributes
+    skip
+    run_generator %w[users name:string email:string]
+    assert_file 'app/interactions/users/signup_interaction.rb' do |f|
+      assert_match(/string :name, :email/, f)
+    end
+  end
+
+  def test_no_test_file_with_skip_test
+    skip
+  end
 end
